3d17d4d999fa3dd36f918f836f7bc918cb72ef47
cubernetes <71025646+cubernetes@users.noreply.github.com>
2025-02-05 01:31:26 +0100

    Update DisableYT.js

--- DisableYT.js
+++ DisableYT.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name Disable YouTube for 20 seconds (Feb 2025)
 // @description Disables the entire html of YouTube for 20 seconds, then shows it again
-// @version	20250205d.1
+// @version	20250205d.2
 // @run-at document-body
 // @include	*://youtube.com/*
 // @include	*://*.youtube.com/*
 // ==/UserScript==
 
 setTimeout(()=>document.body.hidden=false,20000);
-//(()=>{let i = 0;let id=setInterval(()=>{++i;if(i>20)clearInterval(id);document.body.hidden=true},100)})()
 document.body.hidden=true
