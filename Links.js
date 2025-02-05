1bc47f3b1266fd0069fe5548e2fa0dbaf42b5b9f
tsi <timo42@proton.me>
2025-02-05 03:46:53 +0100

    Format links

--- Links.js
+++ Links.js
@@ -1,4 +1,4 @@
-  'https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYT.js',
-  'https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYTHomepageMobile.js',
-  'https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYTRecommendedMobile.js',
-  'https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYTShortsMobile.js',
+https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYT.js
+https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYTHomepageMobile.js
+https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYTRecommendedMobile.js
+https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYTShortsMobile.js
