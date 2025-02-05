a17697e99bec16e92a2c326954b8929b24360074
tsi <timo42@proton.me>
2025-02-05 03:51:27 +0100

    Rename

--- Links.js
+++
@@ -1,4 +0,0 @@
-https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYT.js
-https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYTHomepageMobile.js
-https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYTRecommendedMobile.js
-https://raw.githubusercontent.com/cubernetes/userscripts/refs/heads/main/DisableYTShortsMobile.js
