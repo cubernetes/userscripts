505bf68fc114afdfe3adf3444c88ff9290eae679
tsi <timo42@proton.me>
2025-02-05 03:40:31 +0100

    bump version

--- Wrapper.js
+++ Wrapper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name YouTube Userscript wrapper
 // @description Static wrapper for my other scripts, since userscript plugin of cromite doesn't support URLs, but only files
-// @version	20250205d.1
+// @version	20250205d.2
 // @run-at document-body
 // @include	*://youtube.com/*
 // @include	*://*.youtube.com/*
