365d14a17ef75483e26263608630cb51e19fb43e
tsi <timo42@proton.me>
2025-02-05 08:54:18 +0100

    fix

--- Runner.js
+++ Runner.js
@@ -1,27 +1,33 @@
-function RunForever(outerCallback) {
-	// https://stackoverflow.com/a/14570614
-	const observeDOM = (()=>{
-		const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+// function RunForever(outerCallback) {
+// 	// https://stackoverflow.com/a/14570614
+// 	const observeDOM = (()=>{
+// 		const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+// 
+// 		return (obj, callback)=>{
+// 			if (!obj || !obj.nodeType === true) {
+// 				return;
+// 			}
+// 			if (MutationObserver) {
+// 				const obs = new MutationObserver((mutations)=>{
+// 					if (mutations[0].addedNodes.length) {
+// 						callback(mutations[0]);
+// 						obs.disconnect();
+// 					}
+// 				});
+// 				obs.observe(obj, {childList: true, subtree: true});
+// 			} else if (window.addEventListener) {
+// 				obj.addEventListener('DOMNodeInserted', callback, false);
+// 			}
+// 		}
+// 	})();
+// 
+// 	observeDOM(document, ()=>outerCallback());
+// }
 
-		return (obj, callback)=>{
-			if (!obj || !obj.nodeType === true) {
-				return;
-			}
-			if (MutationObserver) {
-				const obs = new MutationObserver((mutations)=>{
-					if (mutations[0].addedNodes.length) {
-						callback(mutations[0]);
-						obs.disconnect();
-					}
-				});
-				obs.observe(obj, {childList: true, subtree: true});
-			} else if (window.addEventListener) {
-				obj.addEventListener('DOMNodeInserted', callback, false);
-			}
-		}
-	})();
-
-	observeDOM(document, ()=>outerCallback());
+function RunForever(callback) {
+	setInterval(()=>{
+		callback();
+	}, 200);
 }
 
 function RunOnceBody(callback) {
